4cfe283e79fb5dc2051c87f5bc0a35a9cc6e7943
Muhyideen Abdulbasit Kolawole <muhyideenabdulbasit11@gmail.com>
2026-02-01 00:39:21 +0100

    correct prisma 7 setup

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService
@@ -7,9 +10,12 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    super();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    super({ adapter }); // This satisfies the "non-empty" requirement
   }
+
   async onModuleInit() {
     await this.$connect();
   }
