d3d9234086673d92827f5e8245a675ff2712b690
Muhyideen Abdulbasit Kolawole <muhyideenabdulbasit11@gmail.com>
2026-02-26 18:10:36 +0100

    update exam controller

--- src/modules/results/results.module.ts
+++ src/modules/results/results.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { ExamSessionController } from './controllers/exam-session.controller';
 import { ExamSessionService } from './services/exam-session.service';
 import { ExamRepository } from './repositories/exam.repository';
+import { AcademicModule } from '../academic/academic.module';
 
 @Module({
+  imports: [
+    AcademicModule,
+  ],
   controllers: [ExamSessionController],
   providers: [ExamSessionService, ExamRepository],
   exports: [ExamSessionService, ExamRepository],
